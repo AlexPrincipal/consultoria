758eaa5a51c1a7680cab6a08b95fd9d549d04d1c
CHRISTIAN ALEXANDER MUÑOZ ORTIZ <chalmunozor@ittepic.edu.mx>
2025-10-21 04:12:39 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 're

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { Facebook, Linkedin } from 'lucide-react';
-import { FaWhatsapp } from 'react-icons/fa';
+import { FaWhatsapp } from 'react-icons/fa6';
 
 
 export function Footer() {
