f3de4809c64ba04e64d91f0144da3dc240311faf
CHRISTIAN ALEXANDER MUÑOZ ORTIZ <chalmunozor@ittepic.edu.mx>
2025-10-22 19:32:16 +0000

    hice unos ajuste a logo.tsx ve si con eso funciona

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,3 @@
-
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
@@ -8,9 +7,10 @@ export default function Logo({ className }: { className?: string }) {
       src="/logo.png"
       alt="Jurídico Empresarial y Comercial Logo"
       width={960}
-      height={288}
-      className={cn("w-full h-auto object-contain", className)}
+      height={288} // mantiene la relación de aspecto 10:3
+      className={cn("w-auto h-auto object-contain", className)}
       priority
+      sizes="(max-width: 768px) 120px, (max-width: 1024px) 160px, 200px"
     />
   );
 }
