dafdb08a8dd06e2dec39c560621cbda3676e8ad2
CHRISTIAN ALEXANDER MUÑOZ ORTIZ <chalmunozor@ittepic.edu.mx>
2025-11-07 04:55:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3427277360-60843",
+  "appId": "1:815018812396:web:1ed8da39fb91ad36de16c9",
+  "apiKey": "AIzaSyCnF_MlhDwxPx3IO53HnB-3tW03E8nXXek",
+  "authDomain": "studio-3427277360-60843.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "815018812396"
+};
