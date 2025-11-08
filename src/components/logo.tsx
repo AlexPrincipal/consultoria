bfa949e200605cee17a0f2d96a6f730a92761b47
CHRISTIAN ALEXANDER MUÑOZ ORTIZ <chalmunozor@ittepic.edu.mx>
2025-11-08 07:27:46 +0000

    ese no es mi logo , mi logo es logo.png y esta en public

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <img
-      src="https://placehold.co/200x150/242936/FFFFFF?text=C%2B%0AJur%C3%ADdico&font=cinzel"
+      src="/logo.png"
       alt="Jurídico Empresarial y Comercial Logo"
       className="object-contain w-full h-full"
     />
