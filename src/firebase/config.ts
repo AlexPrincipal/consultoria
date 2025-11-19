8067d0bc81a994fafd021afd9ba0210c789c0827
AlexPrincipal <chrisjaog@gmail.com>
2025-11-18 19:47:20 -0700

    Fix Firebase config for Vercel deployment - use environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-3427277360-60843",
-  "appId": "1:815018812396:web:1ed8da39fb91ad36de16c9",
-  "apiKey": "AIzaSyCnF_MlhDwxPx3IO53HnB-3tW03E8nXXek",
-  "authDomain": "studio-3427277360-60843.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "815018812396"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCnF_MlhDwxPx3IO53HnB-3tW03E8nXXek",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-3427277360-60843.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-3427277360-60843",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-3427277360-60843.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "815018812396",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:815018812396:web:1ed8da39fb91ad36de16c9",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
 };
